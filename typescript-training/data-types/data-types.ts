7d420c9027bfd76a67e39f08870a8d698d9bf791
ukalebag <umeshkalebag99@gmail.com>
2026-03-09 11:08:56 +0530

    Assignment 1

---
+++ typescript-training/data-types/data-types.ts
@@ -0,0 +1,79 @@
+//first 
+let tempOfCity: number = 25.5;
+console.log("Temperature of a city in degrees Celsius: " + tempOfCity);
+
+//second
+let customerOrder: boolean = true;
+console.log("Whether a customer has placed an order: " + customerOrder);
+
+//Third
+let personPhone: string = "123-456-7890";
+console.log("Person's phone number: " + personPhone);
+
+//Fouth
+let bankBalance: number = 1000.50;
+console.log("Amount of money in a customer's bank account: " + bankBalance);
+
+//Fifth
+let personEmail: string = "john.doe@example.com";
+console.log("Person's email address: " + personEmail);
+
+//Sixth
+let coordinateOfLocation: number[] = [37.7749, -122.4194];
+console.log("Coordinates of a location (latitude, longitude): " + coordinateOfLocation);
+
+//Seventh
+let personMarritalStatus:boolean = true;
+console.log("Person's marital status: " + personMarritalStatus);
+
+//Eighth
+let personOccupation:string="Software Engineer";
+console.log("Person's occupation: "+ personOccupation);
+
+//Ninth
+let personFavColour:string= "Blue";
+console.log("Person's favourite colour: "+personFavColour);
+
+//Tenth
+let currentYear:number=2023;
+console.log("Current year: " + currentYear);
+
+//Eleventh
+let socialMediaFollowers : number =1000000;
+console.log("Number of followers on a social media platform: " , socialMediaFollowers);
+
+//Twelth
+let ratingOfMovie : number =7.5;
+console.log("Rating of a movie: " + ratingOfMovie);
+
+//Thirteenth
+let personBloodType: string  ='A';
+console.log("Person's blood type: "+ personBloodType);
+
+//Fouteenth
+let titleOfBook : string= "To Kill a Mockingbird";
+console.log("Title of a book: " + titleOfBook);
+
+//Fifteenth
+let noOfEmpInCompany:number = 500;
+console.log("Number of employees in a company: " + noOfEmpInCompany);
+
+//Sixteenth
+let timeOfAnEvent : string ="2:30 PM";
+console.log("Time of an event: " + timeOfAnEvent);
+
+//Seventeenth
+let nameOfCountry : string = "United States";
+console.log("Name of a country: " + nameOfCountry);
+
+//Eighteenth
+let personEyeClr : string = "Brown";
+console.log("Person's eye color: " + personEyeClr);
+
+//Nineteenth
+let personBithPlace : string = "New York City";
+console.log("Person's birthplace: " +personBithPlace);
+
+//Twentith
+let distanceBetnToCities : number =200.5;
+console.log("Distance between two cities: " + distanceBetnToCities);
